6ef029f9ee23985b129c69a6a115cc58d6523f91
George <g.paresishvili@outlook.com>
2022-11-09 13:45:04 +0400

    adsf

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const next = require("next");
 
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+// const dev = process.env.NODE_ENV !== "production";
+// const app = next({ dev });
+const app = next({})
 const handle = app.getRequestHandler();
 
 async function startServer() {
