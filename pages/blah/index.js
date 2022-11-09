b3b37a895c27e76bc8609d7455672100b5e5f762
George <g.paresishvili@outlook.com>
2022-11-09 12:56:12 +0400

    initial

---
+++ pages/blah/index.js
@@ -0,0 +1,3 @@
+export default function Blah() {
+    return(<h1>Blah</h1>)
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Link from "next/link"
+
+export default function Index() {
+    return(<>
+    <h1>asdfasdfa</h1>
+    <Link href={"/blah"}>Blah</Link>
+    </>)
+}
