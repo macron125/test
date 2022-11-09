aca6d1b9e99d867655f2104a703276425d0750fd
George <g.paresishvili@outlook.com>
2022-11-09 14:53:45 +0400

    kjasfhdklasfhdlkasjdf

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const next = require("next");
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 // const app = next({});
-// const handle = app.getRequestHandler();
+const handle = app.getRequestHandler();
 
 // setInterval(() => {
 //     console.log(process.memoryUsage());
