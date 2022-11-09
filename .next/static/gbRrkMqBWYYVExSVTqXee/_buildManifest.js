89826154d129a1af8649a1c01ab843472e9d1eca
George <g.paresishvili@outlook.com>
2022-11-09 13:12:31 +0400

    added build

---
+++ .next/static/gbRrkMqBWYYVExSVTqXee/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-14a77c80e4755560.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],"/blah":["static/chunks/pages/blah-6d20993e53f1e84e.js"],sortedPages:["/","/_app","/_error","/blah"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
