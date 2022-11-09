85361eb4ae43c5046b4534c482437f625c03ab3f
George <g.paresishvili@outlook.com>
2022-11-09 14:11:38 +0400

    asdf

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -5,44 +5,62 @@ exports.id = 888;
 exports.ids = [888];
 exports.modules = {
 
-/***/ 4297:
+/***/ 1203:
 /***/ ((__unused_webpack_module, exports, __webpack_require__) => {
 
 
+
 Object.defineProperty(exports, "__esModule", ({
-    value: true
+  value: true
 }));
 exports["default"] = void 0;
+
 var _async_to_generator = (__webpack_require__(932)/* ["default"] */ .Z);
+
 var _interop_require_default = (__webpack_require__(2648)/* ["default"] */ .Z);
+
 var _react = _interop_require_default(__webpack_require__(6689));
+
 var _utils = __webpack_require__(9232);
+
 function appGetInitialProps(_) {
-    return _appGetInitialProps.apply(this, arguments);
+  return _appGetInitialProps.apply(this, arguments);
 }
+
 function _appGetInitialProps() {
-    _appGetInitialProps = /**
- * `App` component is used for initialize of pages. It allows for overwriting and full control of the `page` initialization.
- * This allows for keeping state between navigation, custom error handling, injecting additional data.
- */ _async_to_generator(function*({ Component , ctx  }) {
-        const pageProps = yield (0, _utils).loadGetInitialProps(Component, ctx);
-        return {
-            pageProps
-        };
-    });
-    return _appGetInitialProps.apply(this, arguments);
+  _appGetInitialProps =
+  /**
+  * `App` component is used for initialize of pages. It allows for overwriting and full control of the `page` initialization.
+  * This allows for keeping state between navigation, custom error handling, injecting additional data.
+  */
+  _async_to_generator(function* ({
+    Component,
+    ctx
+  }) {
+    const pageProps = yield (0, _utils).loadGetInitialProps(Component, ctx);
+    return {
+      pageProps
+    };
+  });
+  return _appGetInitialProps.apply(this, arguments);
 }
+
 var _Component;
+
 class App extends (_Component = _react.default.Component) {
-    render() {
-        const { Component , pageProps  } = this.props;
-        return /*#__PURE__*/ _react.default.createElement(Component, Object.assign({}, pageProps));
-    }
+  render() {
+    const {
+      Component,
+      pageProps
+    } = this.props;
+    return /*#__PURE__*/_react.default.createElement(Component, Object.assign({}, pageProps));
+  }
+
 }
+
 App.origGetInitialProps = appGetInitialProps;
 App.getInitialProps = appGetInitialProps;
-exports["default"] = App; //# sourceMappingURL=_app.js.map
-
+exports["default"] = App;
 
 /***/ }),
 
@@ -67,7 +85,7 @@ module.exports = require("react");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [270], () => (__webpack_exec__(4297)));
+var __webpack_exports__ = __webpack_require__.X(0, [270], () => (__webpack_exec__(1203)));
 module.exports = __webpack_exports__;
 
 })();
